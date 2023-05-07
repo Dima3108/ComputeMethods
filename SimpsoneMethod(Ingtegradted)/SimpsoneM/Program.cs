a7246a97c3f40da0f600e433066ebeb330987a5e
Dima <73338242+Dima3108@users.noreply.github.com>
2023-05-07 17:00:52 +0300

    5

---
+++ SimpsoneMethod(Ingtegradted)/SimpsoneM/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SimpsoneM
+{
+  public  class line_funct : FunctionInterface
+    {
+        public double function(double x) => (4 * x) ;//2x^2   8 16
+    }
+    public class cvadro_func : FunctionInterface
+    {
+        public double function(double x) => 2 * x * x;
+    }
+    public class five_degfunc: FunctionInterface
+    {
+        public double function(double x) => Math.Pow(x, 5.0) / 120.0;
+    }
+    public class free_one_degfunc : FunctionInterface
+    {
+        public double function(double x) => (-3 * Math.Pow(x, 3.0)) + 15 * x;
+    }
+    public class seven_foo_one_degfunc : FunctionInterface
+    {
+        public double function(double x) => (-Math.Pow(x, 7.0)) + (3.0 * Math.Pow(x, 2.0)) - 4 * x;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpsonMethod simpsonMethod = new SimpsonMethod(new line_funct(), 2, 10);
+          Console.WriteLine(  simpsonMethod.ComputeResult() + "\n@\n");
+            SimpsonMethod simpsonMethod1 = new SimpsonMethod(new cvadro_func(), 3, 126);
+            Console.WriteLine(simpsonMethod1.ComputeResult() + "\n@\n");
+            SimpsonMethod simpsonMethod2 = new SimpsonMethod(new five_degfunc(), 21, 7504535.5875);
+            Console.WriteLine(simpsonMethod2.ComputeResult() + "\n@\n");
+            SimpsonMethod simpsonMethod3 = new SimpsonMethod(new free_one_degfunc(), -3, -389897100);
+            Console.WriteLine(simpsonMethod3.ComputeResult() + "\n@\n");
+            SimpsonMethod simpsonMethod4 = new SimpsonMethod(new seven_foo_one_degfunc(), -3, -8935.125);
+            Console.WriteLine(simpsonMethod4.ComputeResult()+"\n@\n");
+        }
+    }
+}
---
+++ SimpsoneMethod(Ingtegradted)/SimpsoneM/SimpsonMethod.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpsoneM
+{
+  public  class SimpsonMethod
+    {
+        public FunctionInterface function;
+        public double a, b;
+        public double f_a;
+        public double c;
+        /// <summary>
+        /// x
+        /// ∫f(x)dx=b
+        /// a
+        /// </summary>
+        /// <param name="function_">интегрируемая функция </param>
+        /// <param name="A">Нижнея граница интегрирования</param>
+        /// <param name="B">результат c</param>
+        public SimpsonMethod(FunctionInterface function_,double A,double B)
+        {
+            function = function_;
+            a = A;
+            b = B;
+            f_a = function.function(a);
+            c = (3 * b) + f_a;
+        }
+        /*
+         * (x/2)(f(a)+f(x))-af(x)+2f((x+a)/2)(x-a)=3b+f(a)
+         */
+        public double ComputeValue(double x)
+        {
+            double f_x = function.function(x);
+            double f_xa = function.function((x + a) / 2);
+            return ((x / 2) * (f_a + f_x)) - (a * f_x) + ((2 * f_xa) * (x - a));
+
+        }
+        public double DeviationEstimate(double d_b) =>Math.Abs( ( d_b/b) - 1.00000000);
+        public double SegmentationCalculation(double start_val,double max_val,int crushing_step,double eps)
+        {
+ double offset = (max_val - start_val) / crushing_step;
+            double delta_0 = DeviationEstimate(ComputeValue(start_val + offset));
+                //Math.Abs( DeviationEstimate(ComputeValue(start_val))-DeviationEstimate(ComputeValue(start_val+offset)));
+            double val = start_val;
+            int step = 0;
+            /* if (delta_0 <= eps)
+                 return start_val;*/
+#if DEBUG
+            Console.WriteLine($"of:{offset}");
+#endif
+            if (offset < eps)
+            {
+                if(DeviationEstimate(ComputeValue(start_val))> DeviationEstimate(ComputeValue(start_val + offset))){
+                    return start_val + offset;
+                }
+                else
+                {
+                    return start_val;
+                }
+            }
+
+            for(int i = 1; i < crushing_step - 1; i++)
+            {
+                double delta = //Math.Abs(DeviationEstimate(ComputeValue(start_val + (offset * i))) - 
+                    DeviationEstimate(ComputeValue(start_val + (offset * (i + 1))))
+                    //)
+                    ;
+                if (delta < delta_0)
+                {
+                    delta_0 = delta;
+                    step = i;
+
+                }
+            }
+#if DEBUG
+            Console.WriteLine($"dl:{DeviationEstimate(ComputeValue(start_val + (offset * step)))}-{DeviationEstimate(ComputeValue(start_val + (offset * (step+1))))}");
+#endif
+            return SegmentationCalculation(start_val + (offset * step), start_val + (offset * (step + 1)), crushing_step, eps);
+        }
+        public const double eps= 0.00000000005;
+        public const int step = 1024+512+256+12;
+        public double ComputeResult()
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+            else
+            {
+                double v = -2.3456789;
+                bool is_cvad_function = function.function(v) == function.function(-v);
+                bool is_lin_func;
+                double[] r = new double[] { function.function(-1.00123), function.function(-1.00123 + 3.0456), function.function(8.0009345) };
+                is_lin_func = (r[0] <= r[1] && r[1] <= r[2]) || (r[0] > r[1] && r[1] > r[2]);
+                bool is_vozr = r[0] < r[1] && r[1] < r[2];
+                if (b > 0)
+                {
+                    if (is_cvad_function || is_lin_func)
+                    {
+#if DEBUG
+                        Console.WriteLine(is_vozr);
+                        Console.WriteLine(f_a < b);
+#endif
+                        if ((is_vozr && f_a < b)||(!is_vozr&&b>f_a))
+                        {
+                            return SegmentationCalculation(a, short.MaxValue, step, eps);
+                        }
+                        else
+                        {
+                            return SegmentationCalculation(short.MinValue, a, step, eps);
+                        }
+                    }
+                    else return SegmentationCalculation(short.MinValue, short.MaxValue, step, eps);
+                    throw new Exception();
+                        
+                }
+                else
+                {
+                    if (is_cvad_function || is_lin_func)
+                    {
+                        if ((!is_vozr && f_a < b) || (is_vozr && b > f_a))
+                        {
+                            return SegmentationCalculation(a, short.MaxValue, step, eps);
+                        }
+                        else
+                        {
+                            return SegmentationCalculation(short.MinValue, a, step, eps);
+                        }
+                    }
+                    else return SegmentationCalculation((double)short.MinValue, (double)short.MaxValue, step, eps);
+                    throw new Exception();
+                }
+            }
+        }
+        /*{
+            double dlt = d_b / b;
+            return dlt - 1.0;
+        }*/
+
+    }
+}
